ac46288b83985885f1a1fb94967d3f52d8e6185c
Xabi Ezpeleta <xezpeleta@gmail.com>
2014-11-21 00:21:46 +0100

    moved script

---
+++ utils/download-Win7-vm.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+curl -O -L "https://www.modern.ie/vmdownload?platform=mac&virtPlatform=virtualbox&browserOS=IE11-Win7&parts=4&filename=VMBuild_20131127/VirtualBox/IE11_Win7/Mac/IE11.Win7.For.MacVirtualBox.part{1.sfx,2.rar,3.rar,4.rar}"
+
+chmod +x IE11.Win7.For.MacVirtualBox.part1.sfx
+
+./IE11.Win7.For.MacVirtualBox.part1.sfx
