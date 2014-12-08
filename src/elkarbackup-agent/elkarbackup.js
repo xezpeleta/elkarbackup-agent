0c1d57bcc020c4841a172d19a2d1b450b42e6da7
Xabi Ezpeleta <xezpeleta@gmail.com>
2014-12-08 19:54:15 +0100

    Add new class elkarbackup

---
+++ src/elkarbackup-agent/elkarbackup.js
@@ -0,0 +1,9 @@
+Elkarbackup = function(){
+};
+
+// Download SSH key from ElkarBackup server
+// and save it to C:\cygwin\home\IEUser\.ssh
+Elkarbackup.importSshId = function(callback){
+};
+
+module.exports = Elkarbackup;
