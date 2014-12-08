dcedd182588a81f6b23632c9b873f2bfd97f1b1d
Xabi Ezpeleta <xezpeleta@gmail.com>
2014-12-08 19:45:20 +0100

    Network config script for win7 and win8

---
+++ utils/configure-ip-address-win7.bat
@@ -0,0 +1,1 @@
+netsh int ip set address "Local Area Connection 3" static 192.168.33.21 255.255.255.0
---
+++ utils/configure-ip-address-win8.bat
@@ -0,0 +1,1 @@
+netsh interface ipv4 set address name="Ethernet 3" source=static address=192.168.33.21 mask=255.255.255.0
